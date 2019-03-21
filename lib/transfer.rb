4a6010dfc717790d0a663ad65734fc0a03c1317f
Igor Rud <rud.igor23@gmail.com>
2019-03-21 18:07:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,8 +3,8 @@ class Transfer
 attr_accessor :sender, :receiver, :amount
 
 def initialize(sender, receiver, amount)
-  @sender = sender
-  @receiver = receiver
+  @sender = BankAccount.name
+  @receiver = BankAccount.name
   @amount = amount
 @status = "pending"
 end
