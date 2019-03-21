50f32c7db0e142513f413941982e6382f4b82065
Igor Rud <rud.igor23@gmail.com>
2019-03-21 16:03:14 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,12 +1,12 @@
 class BankAccount
-attr_accessor :name, :balance, :status
-
+attr_accessor :name, :balance
+attr_reader :status
 def initialize(name)
   @name = name
   balance = 1000
   @balance = balance
   status = "open"
-  @status = status  
+  @status = status
 end
 
 
